6ec4f5c2c7a588bc085f12e2b82b8f4eed8f3edc
ryan <ryantxu@gmail.com>
2015-09-28 09:56:43 -0700

    move jackson into the main jar file

---
+++ src/main/java/com/spatial4j/jackson/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Databind for Jackson
+ */
+package com.spatial4j.jackson;
---
+++ src/test/java/com/spatial4j/jackson/SpatialMapperTest.java
@@ -0,0 +1,15 @@
+package com.spatial4j.jackson;
+
+import org.junit.Test;
+
+public class SpatialMapperTest
+{
+  @Test
+  public void test_writeDatesAsTimestamps_property()
+  {
+    Spatial4jMapper objectUnderTest = new Spatial4jMapper();
+
+    // TODO...
+      
+  }
+}
